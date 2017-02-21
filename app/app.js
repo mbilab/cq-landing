525c778deacbf66723a5899dbec7d29cec6bcd85
wei831127 <wei831127@yahoo.com.au>
2017-02-21 20:51:34 +0800

    add CQ element

--- app/app.js
+++ app/app.js
@@ -18,8 +18,12 @@ $(document).ready(function() {
   $('body').css('background-image','url('+require('./res/cq-res/home/background.png')+')')
   $('#title').css('background-image','url('+require('./res/cq-res/main/logo-vertical.png')+')')
   $('#iphone').css('background-image','url('+require('./res/cq-res/home/iphone.png')+')')
-  $('#register-btn').css('background-image','url('+require('./res/cq-res/home/register-button.png')+')')
+  // $('#register-btn').css('background-image','url('+require('./res/cq-res/home/register-button.png')+')')
   $('#app').css('background-image','url('+require('./res/cq-res/home/app.png')+')')
   $('#google').css('background-image','url('+require('./res/cq-res/home/google.png')+')')
+
+  $('#content-top-bg').css('background-image','url('+require('./res/cq-res/public_use/content-top-bg.png')+')')
+  $('#content-mid-bg').css('background-image','url('+require('./res/cq-res/public_use/content-bg.png')+')')
+  $('#content-bottom-bg').css('background-image','url('+require('./res/cq-res/public_use/content-bottom-bg.png')+')')
 
 })
