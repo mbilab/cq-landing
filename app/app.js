251cd6fee31711ceefd7fe76b408cc2d9b1a09a9
wei831127 <wei831127@yahoo.com.au>
2017-02-23 20:26:01 +0800

    import image to css

--- app/app.js
+++ app/app.js
@@ -14,14 +14,4 @@ $(document).ready(function() {
   $('#tartew-btn').click(function() {
     window.open('https://www.youtube.com/watch?v=T4SimnaiktU')
   })
-
-  $('body').css('background-image','url('+require('./res/cq-res/home/background.png')+')')
-  $('#title').css('background-image','url('+require('./res/cq-res/main/logo-vertical.png')+')')
-  $('#iphone').css('background-image','url('+require('./res/cq-res/home/iphone.png')+')')
-  $('#app').css('background-image','url('+require('./res/cq-res/home/app.png')+')')
-  $('#google').css('background-image','url('+require('./res/cq-res/home/google.png')+')')
-
-  $('#content-top-bg').css('background-image','url('+require('./res/cq-res/public_use/content-top-bg.png')+')')
-  $('#content-mid-bg').css('background-image','url('+require('./res/cq-res/public_use/content-bg.png')+')')
-  $('#content-bottom-bg').css('background-image','url('+require('./res/cq-res/public_use/content-bottom-bg.png')+')')
 })
