eb4287a54d931ba8c5f5e4a36606c3702686ed18
wei831127 <wei831127@yahoo.com.au>
2017-03-08 14:12:46 +0800

    use rem to resize

--- app/app.js
+++ app/app.js
@@ -5,6 +5,15 @@ window.$ = window.jQuery = require('jquery')
 require('semantic-ui/dist/semantic.js')
 
 $(document).ready(function() {
+  const w = 1280
+  const h = 720
+  const unit = 14
+  $(window).resize(function(){
+    let areaRatio = ($(window).width()) * ($(window).height()) / (w * h)
+    console.log(`${w},${h},${areaRatio}`)
+    console.log(unit * areaRatio)
+    $('body').css('font-size', `${unit * areaRatio}px`)
+  })
   $('.android.icon').click(function() {
     window.open('https://play.google.com/store/apps/details?id=com.mbilab.cq')
   })
@@ -14,7 +23,4 @@ $(document).ready(function() {
   $('#start-btn').click(function() {
     window.open('https://www.youtube.com/watch?v=T4SimnaiktU')
   })
-  wdth=$(window).width()
-  $(window).resize(function() {
-   })
 })
