251807563ce839f7723b9e451a8bbf9fa70e8a81
dirty <darby.chang@gmail.com>
2017-03-08 13:58:01 +0800

    refine webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,60 +1,34 @@
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
 
 module.exports = {
   devServer: {
-    contentBase: path.join(__dirname, 'dist'), //! path needed?
     host: '0.0.0.0',
-    inline: true,
-    stats: { //! all needed?
-      assets: false,
-      children: false,
-      chunks: false,
-      colors: true,
-      hash: false,
-      modules: false,
-      timings: false,
-      version: false,
-      warnings: false,
-    },
+    stats: 'minimal',
   },
   entry: './app/app.js',
   module: {
-    rules: [ //! order
-      {
-        test: /\.css$/, //! needed?
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: 'css-loader',
-        }),
-      },
+    rules: [
+      { test: /\.css$/, use: ExtractTextPlugin.extract('css-loader') },
+      { test: /\.(eot|ico|jpg|mp3|svg|ttf|woff2|woff|png)($|\?)/, loader: 'file-loader' },
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['es2015']} },
       {
         test: /\.sass$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
           use: ['css-loader', 'sass-loader'],
         }),
       },
-      { test: /\.(eot|ico|jpg|mp3|svg|ttf|woff2|woff|png?)($|\?)/, loader: 'file-loader' }, //! correct?
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader', query: { presets: ['es2015']} }, //! correct?
-      {
-        test: /\.pug$/,
-        use: ['html-loader', 'pug-html-loader'],
-      },
+      { test: /\.pug$/, use: ['html-loader', 'pug-html-loader'] },
     ],
   },
   output: {
     filename: 'app.js',
-    path: path.resolve('dist'), //! path needed?
+    path: __dirname+'/dist',
   },
   plugins: [
-    new ExtractTextPlugin({
-      allChunks: true, //! needed?
-      disable: false, //! needed?
-      filename: 'style.css', //! app.sass & app.css
-    }),
+    new ExtractTextPlugin({ filename: 'app.css' }),
     new HtmlWebpackPlugin({ template: './app/index.pug' }),
   ],
 }
+
 //- vi:sw=2:ts=2:et
