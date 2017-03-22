367d3bd4bcf7a4bc66345b1d60183e58326b1c6c
wei831127 <wei831127@yahoo.com.au>
2017-03-22 23:33:04 +0800

    accomplish basic architecture

--- app/build.js
+++ app/build.js
@@ -1,1 +1,12 @@
+import 'semantic-ui/dist/semantic.css'
 import './build.sass'
+
+window.$ = window.jQuery = require('jquery')
+require('semantic-ui/dist/semantic.js')
+
+$(document).ready(function() {
+  $('.ui.big.heart.rating').rating({
+    initialRating: 1,
+    maxRating: 5,
+  })
+})
