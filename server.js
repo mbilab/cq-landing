efa43be40ef90bef7a9b06121d6910e29ac82e3d
wei831127 <wei831127@yahoo.com.au>
2017-07-18 15:15:32 +0800

    add schema

--- server.js
+++ server.js
@@ -7,9 +7,43 @@ const Schema = mongoose.Schema
 server.listen(8034)
 
 const player = new Schema({
-  username: String,
+  id: String,
+  name: String,
+  account: String,
+  password: Object,
+  level: Number,
+  exp: Number,
+  title: Object,
+  personal: String,
+  character: Object,
+  skillPoint: Object,
+  buff: Object,
+  time: Object,
+  bonus: Object,
+  ext: Object
+})
+
+const playerQuest = new Schema({
+  pid: String,
+  qid: String,
+  completeTime: String,
+  accepted: Number,
+  alreadyRead: Number,
+  memo: String
+})
+
+const playerSkill = new Schema({
+  pid: String,
+  sid: String,
   level: Number
 })
+
+const playerTitle = new Schema({
+  pid: String
+  tid: String
+  completeTime: String
+})
+
 
 mongoose.connect(
   'mongodb://' + config.user + ':' + config.password + '@' + config.host + '/' + config.database,
@@ -24,9 +58,11 @@ db.on('error', function(err) {
 db.on('open', function() {
   console.log('db open')
   const playerModel = mongoose.model('player', player)
+  const playerQuestModel = mongoose.model('player.quest', playerQuest)
+  const playerSkillModel = mongoose.model('player.skill', playerSkill)
+  const playerTitleModel = mongoose.model('player.title', playerTitle)
+}
 
-  new playerModel({username: 'aaa', level: 10}).save()
-})
 
 io.on('connection', function(socket) {
 
